2568af05a4563b5889527e0b3b1575389d168c4a
unknown <chanthamengkong@gmail.com>
2025-01-03 12:18:11 +0700

    updated LoginUI.java

--- IdeaProjects/libraryProject/src/ui/LoginUI.java
+++ IdeaProjects/libraryProject/src/ui/LoginUI.java
@@ -2,20 +2,29 @@ package ui;
 import services.Admin;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+import java.awt.event.*;
 
 public class LoginUI extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
 
     LoginUI() {
         this.setSize(1000, 700);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Login Page");
+        cardLayout =new CardLayout();
+        cardPanel=new JPanel(cardLayout);
 
-        JPanel mainPanel = new JPanel(new BorderLayout());
-
-
+        cardPanel.add(CreateLoginForm(),"Login");
+        cardPanel.add(CreateRegistrationForm(),"Registration");
+        this.add(cardPanel);
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+        this.setVisible(true);
+        this.setResizable(false);
+    }
+    private JPanel CreateLoginForm(){
+        JPanel loginContainer = new JPanel(new BorderLayout());
         ImageIcon imageIcon = new ImageIcon("src/images/thumbnails/library.jpg");
         Image scaledImage = imageIcon.getImage().getScaledInstance(600, 500, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
@@ -23,12 +32,14 @@ public class LoginUI extends JFrame {
 
         JPanel imgPanel = new JPanel();
         imgPanel.add(imgLabel);
+        JPanel loginPanel =new JPanel();
+        loginPanel.setLayout(new BoxLayout(loginPanel,BoxLayout.Y_AXIS));
+        loginPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        JPanel loginPanel = new JPanel();
-        loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.Y_AXIS));
-        loginPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Padding
-
-        JLabel titleLabel = new JLabel("Login", SwingConstants.CENTER);
+        JLabel titleLabel = new JLabel("Login");
+        JLabel welcome=new JLabel("Welcome to Imagine Library");
+        welcome.setFont(new Font("Arial", Font.BOLD, 24));
+        welcome.setAlignmentX(Component.CENTER_ALIGNMENT);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
@@ -45,10 +56,25 @@ public class LoginUI extends JFrame {
         JButton loginButton = new JButton("Login");
         loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        JLabel result = new JLabel();
-        result.setAlignmentX(Component.CENTER_ALIGNMENT);
-        result.setFont(new Font("Arial", Font.PLAIN, 16));
-        result.setForeground(Color.BLUE);
+        JPanel createAccPanel = new JPanel();
+        createAccPanel.setLayout(new BoxLayout(createAccPanel,BoxLayout.Y_AXIS));
+        createAccPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        createAccPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+
+        JLabel crateAccText = new JLabel("Don't have account?");
+        JLabel registerLink =new JLabel("Create one");
+        crateAccText.setAlignmentX(Component.CENTER_ALIGNMENT);
+        registerLink.setAlignmentX(Component.CENTER_ALIGNMENT);
+        registerLink.setForeground(Color.BLUE);
+        registerLink.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        createAccPanel.add(crateAccText);
+        createAccPanel.add(registerLink);
+        registerLink.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                cardLayout.show(cardPanel,"Registration");
+            }
+        });
 
         loginButton.addActionListener(e -> {
             String inputEmail = emailField.getText().trim();
@@ -95,25 +121,144 @@ public class LoginUI extends JFrame {
                 }
             }
         });
-
-
         loginPanel.add(titleLabel);
-        loginPanel.add(Box.createVerticalStrut(20));
+        loginPanel.add(Box.createVerticalStrut(10));
+        loginPanel.add(welcome);
+        loginPanel.add(Box.createVerticalStrut(10));
+        loginPanel.add(welcome);
+        loginPanel.add(Box.createVerticalStrut(10));
         loginPanel.add(emailField);
         loginPanel.add(Box.createVerticalStrut(10));
         loginPanel.add(passwordField);
-        loginPanel.add(Box.createVerticalStrut(20));
+        loginPanel.add(Box.createVerticalStrut(10));
         loginPanel.add(loginButton);
-        loginPanel.add(Box.createVerticalStrut(20));
-        loginPanel.add(result);
+        loginPanel.add(Box.createVerticalStrut(10));
+        loginPanel.add(createAccPanel);
+        loginPanel.add(Box.createVerticalStrut(10));
+        loginPanel.add(loginButton);
 
-        mainPanel.add(imgPanel, BorderLayout.WEST);
-        mainPanel.add(loginPanel);
+        loginContainer.add(imgPanel,BorderLayout.WEST);
+        loginContainer.add(loginPanel,BorderLayout.CENTER);
 
-        this.add(mainPanel);
-        this.setFocusable(true);
-        this.requestFocusInWindow();
-        this.setVisible(true);
+        return loginContainer;
+    }
+    private JPanel CreateRegistrationForm(){
+        ImageIcon imageIcon = new ImageIcon("src/images/thumbnails/library.jpg");
+        Image scaledImage = imageIcon.getImage().getScaledInstance(600, 500, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        JLabel imgLabel = new JLabel(scaledIcon);
+
+        JPanel imgPanel = new JPanel();
+        imgPanel.add(imgLabel);
+
+        JPanel registrationContainer=new JPanel(new BorderLayout());
+        JPanel registerPanel=new JPanel();
+        registerPanel.setLayout(new BoxLayout(registerPanel,BoxLayout.Y_AXIS));
+        registerPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        JLabel titleLabel = new JLabel("Registration");
+        titleLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel welcome=new JLabel("Welcome to Imagine Library");
+        welcome.setFont(new Font("Arial", Font.PLAIN, 16));
+        welcome.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JTextField nameField=new JTextField();
+        JTextField emailField = new JTextField();
+        JPasswordField passwordField = new JPasswordField();
+
+
+        addPlaceholder(nameField,"Enter your name");
+        addPlaceholder(emailField, "Enter your email");
+        addPasswordPlaceholder(passwordField, "Enter your password");
+        nameField.setMaximumSize(new Dimension(300,30));
+        emailField.setMaximumSize(new Dimension(300, 30));
+        passwordField.setMaximumSize(new Dimension(300, 30));
+
+        JLabel backToLogin=new JLabel("Back to login");
+        backToLogin.setAlignmentX(Component.CENTER_ALIGNMENT);
+        backToLogin.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        backToLogin.setForeground(Color.BLUE);
+        backToLogin.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+               cardLayout.show(cardPanel,"Login");
+            }
+        });
+
+        JButton registerBtn =new JButton("Register");
+        registerBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        registerBtn.addActionListener(e -> {
+            String inputName=nameField.getText().trim();
+            String inputEmail = emailField.getText().trim();
+            char[] inputPassword = passwordField.getPassword();
+            if(inputName.isEmpty() || inputName.equals("Enter your name") ){
+                JOptionPane.showMessageDialog(this,"Please enter your real name","error",JOptionPane.ERROR_MESSAGE);
+            }
+
+            else if (inputEmail.isEmpty() || inputEmail.equals("Enter your email") ) {
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Please enter an email!",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            } else if (!isValidEmail(inputEmail)) {
+                JOptionPane.showMessageDialog(
+                        LoginUI.this,
+                        "Please enter a valid email address!(email should be followed by @gmail.com)",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            } else if (inputPassword.length == 0 || new String(inputPassword).equals("Enter your password")) {
+                JOptionPane.showMessageDialog(
+                        LoginUI.this,
+                        "Please enter a password!",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            } else{
+                Admin admin= new Admin();
+                String passwordString=new String(inputPassword);
+                if(admin.verifyAdmin(inputEmail,passwordString)){
+                    JOptionPane.showMessageDialog(
+                            LoginUI.this,
+                            "Welcome, Admin!",
+                            "Success",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                }else {
+                    JOptionPane.showMessageDialog(
+                            LoginUI.this,
+                            "Invalid credentials. Please try again.",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE
+                    );
+                }
+            }
+        });
+
+
+        registerPanel.add(titleLabel);
+        registerPanel.add(Box.createVerticalStrut(10));
+        registerPanel.add(welcome);
+        registerPanel.add(Box.createVerticalStrut(10));
+        registerPanel.add(nameField);
+        registerPanel.add(Box.createVerticalStrut(10));
+        registerPanel.add(emailField);
+        registerPanel.add(Box.createVerticalStrut(10));
+        registerPanel.add(passwordField);
+        registerPanel.add(Box.createVerticalStrut(10));
+        registerPanel.add(registerBtn);
+        registerPanel.add(Box.createVerticalStrut(20));
+        registerPanel.add(backToLogin);
+        registrationContainer.add(imgPanel,BorderLayout.WEST);
+        registrationContainer.add(registerPanel,BorderLayout.CENTER);
+
+        return registrationContainer;
     }
 
     private void addPlaceholder(JTextField field, String placeholder) {
@@ -168,6 +313,7 @@ public class LoginUI extends JFrame {
         String emailRegex = "^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$"; // email validation
         return email.matches(emailRegex);
     }
+
 
     public static void main(String[] args) {
         new LoginUI();
