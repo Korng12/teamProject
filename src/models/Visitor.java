0e58e75e07bc719c2d970b764ef1c302589ab315
Korng12 <chanthamengkong@gmail.com>
2025-01-20 16:36:45 +0700

    Delete src directory

--- src/models/Visitor.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class Visitor extends User{
-    public Visitor(String name, String email, String password,String imgPath) {
-        super(name, email, password,imgPath);
-        this.role = "visitor";
-    }
-
-}
